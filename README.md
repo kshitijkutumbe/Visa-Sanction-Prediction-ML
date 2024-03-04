08004cb49c7e12f7b0a532e4df283fa9dcd58e43
kshitijkutumbe <kshitijkutumbe@gmail.com>
2024-03-05 01:36:11 +0530

    added setup.py

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # US-Visa-Approval-Prediction
 
-## Live matarials docs
-
-[link](https://docs.google.com/document/d/1UFiHnyKRqgx8Lodsvdzu58LbVjdWHNf-uab2WmhE0A4/edit?usp=sharing)
-
-
 ## Git commands
 
 ```bash
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "us_visa_prediction",
+    version = "0.0.0",
+    author = "Kshitij Kutumbe",
+    author_email =  'kshitijkutumbe@gmail.com',
+    packages =  find_packages()
+)
