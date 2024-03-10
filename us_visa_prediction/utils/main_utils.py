89c15c76bd2ca187c7c868a34a941c16fdbd44b9
kshitijkutumbe <kshitijkutumbe@gmail.com>
2024-03-10 14:21:38 +0530

    modified main_utils

--- us_visa_prediction/utils/main_utils.py
+++ us_visa_prediction/utils/main_utils.py
@@ -6,8 +6,8 @@ import dill
 import yaml
 from pandas import DataFrame
 
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
+from us_visa_prediction.exception import USvisaException
+from us_visa_prediction.logger import logging
 
 
 def read_yaml_file(file_path: str) -> dict:
