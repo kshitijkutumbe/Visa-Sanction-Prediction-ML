680622c93a23da30bf2852b6ffa4f1aa4b8a4819
kshitijkutumbe <kshitijkutumbe@gmail.com>
2024-03-17 00:58:50 +0530

    added cicd

--- us_visa_prediction/configuration/aws_connection.py
+++ us_visa_prediction/configuration/aws_connection.py
@@ -16,12 +16,12 @@ class S3Client:
         """
 
         if S3Client.s3_resource==None or S3Client.s3_client==None:
-            __access_key_id = os.getenv("AWS_ACCESS_KEY_ID_ENV_KEY" )
-            __secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY_ENV_KEY" )
+            __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY)
+            __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY)
             if __access_key_id is None:
-                raise Exception(f"Environment variable: {__access_key_id} is not not set.")
+                raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not not set.")
             if __secret_access_key is None:
-                raise Exception(f"Environment variable: {__secret_access_key} is not set.")
+                raise Exception(f"Environment variable: {AWS_SECRET_ACCESS_KEY_ENV_KEY} is not set.")
         
             S3Client.s3_resource = boto3.resource('s3',
                                             aws_access_key_id=__access_key_id,
